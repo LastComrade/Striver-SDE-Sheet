34e6fa45a787cdec1886eaee3a90477c94924ae3
Konark Lohat <konarklohat123456@gmail.com>
2022-06-14 00:11:44 +0530

    Day3 -> Reverse pairs

---
+++ Day-03 (Arrays Part - 3)/Reverse pairs.cpp
@@ -0,0 +1,62 @@
+// Problem Link - https://leetcode.com/problems/reverse-pairs
+
+class Solution
+{
+  void _merge(vector<int> &nums, int low, int mid, int high, int &reversePairCount)
+  {
+    int leftInd = low, rightInd = mid + 1;
+    int r = rightInd, temp[high - low + 1], tempInd = 0, counter = 0;
+
+    // 4 6 8,  1 2 3
+    while (leftInd <= mid)
+    {
+      while (r <= high && (nums[leftInd] > (long)2 * nums[r]))
+        counter++, r++;
+
+      reversePairCount += counter;
+
+      while (rightInd <= high && nums[rightInd] <= nums[leftInd])
+      {
+        temp[tempInd++] = nums[rightInd++];
+      }
+
+      temp[tempInd++] = nums[leftInd++];
+    }
+
+    while (rightInd <= high)
+    {
+      temp[tempInd++] = nums[rightInd++];
+    }
+
+    for (int i = 0; i < tempInd; i++)
+      nums[low++] = temp[i];
+  }
+
+  void _mergeSort(vector<int> &nums, int low, int high, int &reversePairCount)
+  {
+    if (low < high)
+    {
+      int mid = low + (high - low) / 2;
+      _mergeSort(nums, low, mid, reversePairCount);
+      _mergeSort(nums, mid + 1, high, reversePairCount);
+      _merge(nums, low, mid, high, reversePairCount);
+    }
+  }
+
+public:
+  int reversePairs(vector<int> &nums)
+  {
+    int reversePairCount = 0;
+    int sz = nums.size();
+
+    if (sz <= 1)
+      return reversePairCount;
+
+    _mergeSort(nums, 0, sz - 1, reversePairCount);
+
+    return reversePairCount;
+  }
+};
+
+// Time Complexity: O(N * log(N))
+// Space Complexity: O(N)
